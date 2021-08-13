22ea0da9fa828630fb45bcb30809fded69b9a2e3
marlageha <marla.geha@yale.edu>
2021-08-13 13:20:07 -0400

    Update dmost_emcee.py

--- dmost_emcee.py
+++ dmost_emcee.py
@@ -131,9 +131,7 @@ def mk_emcee_plots(pdf, slits, nexp, arg, sampler, wave, flux, model):
     
     fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(20,5))
 
-
-    tau = sampler.get_autocorr_time()
-    burnin = int(2 * np.max(tau))
+    burnin = slits['emcee_burn'][arg,nexp]
 
     for ii in range(20):
         ax1.plot(sampler.chain[ii,:,0], color="k",linewidth=0.5)
