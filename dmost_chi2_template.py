36d85a6755cb458c3491d4adf45fc4bac96e2d58
marlageha <mgeha@Marlas-MBP.wireless.yale.internal>
2022-01-13 15:13:03 -0500

    wavelength error

--- dmost_chi2_template.py
+++ dmost_chi2_template.py
@@ -32,11 +32,12 @@ def create_chi2_masks(data_wave):
     
     # DETERMINE CHI2 NEAR STRONG LINES
     cmask1 = (data_wave > 6200) & (data_wave < 6750) 
-    cmask4 = (data_wave > 6950) & (data_wave < 7140) 
-    cmask5 = (data_wave > 7350) & (data_wave < 7550) 
-    cmask2 = (data_wave > 7800) & (data_wave < 8125) 
-    cmask3 = (data_wave > 8350) & (data_wave < 8850) 
-    chi2_mask = cmask1 | cmask2 | cmask3 | cmask4 | cmask5
+    cmask2 = (data_wave > 6950) & (data_wave < 7140) 
+    cmask3 = (data_wave > 7350) & (data_wave < 7550) 
+    cmask4 = (data_wave > 7800) & (data_wave < 8125) 
+    cmask5 = (data_wave > 8170) & (data_wave < 8210) 
+    cmask6 = (data_wave > 8350) & (data_wave < 8850) 
+    chi2_mask = cmask1 | cmask2 | cmask3 | cmask4 | cmask5 | cmask6
 
 
     # USE THIS FOR CONTINUUM FITTING
