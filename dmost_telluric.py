a1fb21b1ab6161b18cc1f4c4449ebaf473e3c337
marlageha <mgeha@Marlas-MBP.wireless.yale.internal>
2022-01-21 10:38:28 -0500

    update plotting

--- dmost_telluric.py
+++ dmost_telluric.py
@@ -515,13 +515,15 @@ def final_telluric_values(data_dir, slits, mask, nexp, hdu):
     ax2.axhline(final_o2)
     ax2.set_ylim(0.6,2.05)
 
+
+    airmass_fit = 'Airmass fit value {:0.3f}'.format(get_o2_nodata(mask['airmass'][nexp]))
     ax4.plot(fslits2['rSN'][:,nexp],fslits2['telluric_o2'][:,nexp],'.')
     ax4.errorbar(fslits2['rSN'][m,nexp],fslits2['telluric_o2'][m,nexp],\
               yerr=np.abs(fslits2['telluric_o2_err'][m,nexp]),fmt='.r',ecolor='grey')
 
     ax4.set_title('Full data range: O2 = {:0.3f}'.format(final_o2))
-    ax4.axhline(final_o2)
-    
+    ax4.axhline(final_o2,label=airmass_fit)
+    ax4.legend()
 
 
     ax1.set_xlabel('SN')
