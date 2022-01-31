998b6f462f92389a69c8ddc6587113bc61571181
marlageha <mgeha@marlas-mbp.lan>
2022-01-31 17:48:35 -0500

    chip gap fix

--- dmost_chi2_template.py
+++ dmost_chi2_template.py
@@ -346,7 +346,7 @@ def run_chi2_templates(data_dir, slits, mask, clobber=0):
 
             jhdu = fits.open(data_dir+'collate1d/'+obj['collate1d_filename'])
 
-            jwave,jflux,jivar, SN = dmost_utils.load_coadd_collate1d(jhdu) 
+            jwave,jflux,jivar, SN = dmost_utils.load_coadd_collate1d(obj,jhdu) 
             vexp = 0
             if (obj['reduce_flag'][0] == 0):
                 m=obj['reduce_flag'] != 0
