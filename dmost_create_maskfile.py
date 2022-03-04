4179b2cb373e2f853eed22c5a46b1ae33575c6a3
marlageha <mgeha@Marlas-MBP.wireless.yale.internal>
2022-03-04 12:54:29 -0500

    add int to det

--- dmost_create_maskfile.py
+++ dmost_create_maskfile.py
@@ -483,17 +483,21 @@ def add_spec1d_fileinfo(data_dir,slits,mask,nexp):
     
             m1,m2,dd = sm.spherematch(slits['RA'], slits['DEC'],[slit_header['RA']],[slit_header['DEC']],2./3600)
             
+            det = slit_header['DET']
+            if (type(slit_header['DET']) == 'str':
+                tmp = slit_header['DET'].split('DET')
+                det = int(tmp[1]) 
             
-            if (np.size(m1) > 0) & (int(slit_header['DET']) < 5):
+            if (np.size(m1) > 0) & (det < 5):
                 arg=m1[0]
-                slits['bdet'][arg,ii]       = slit_header['DET']
+                slits['bdet'][arg,ii]       = det
                 slits['bslit'][arg,ii]      = slit_header['SLITID']
                 slits['bspat'][arg,ii]      = round(slit_header['HIERARCH SPAT_PIXPOS'])
                 slits['rms_arc_b'][arg,ii]  = slit_header['WAVE_RMS']
 
-            if (np.size(m1) > 0) & (int(slit_header['DET']) >= 5):
+            if (np.size(m1) > 0) & (det >= 5):
                 arg=m1[0]
-                slits['rdet'][arg,ii]       = slit_header['DET']
+                slits['rdet'][arg,ii]       = det
                 slits['rslit'][arg,ii]      = slit_header['SLITID']
                 slits['rspat'][arg,ii]      = round(slit_header['HIERARCH SPAT_PIXPOS'])
                 slits['rms_arc_r'][arg,ii]  = slit_header['WAVE_RMS']
