04bbe12daa68ec3fe86100ac430f1bf218e95735
marlageha <marla.geha@yale.edu>
2022-05-19 12:33:03 -0400

    Update __init__.py

--- dmost/__init__.py
+++ dmost/__init__.py
@@ -1,4 +1,4 @@
 """
 This is the top directory of the dmost package
 """
-from .core import dmost_utils,dmost_create_maskfile
+from .core import dmost_utils,dmost_create_maskfile,dmost_flexure,dmost_telluric,dmost_chi2_template
