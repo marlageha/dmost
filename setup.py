aef4a0d5505b922ac9fbfa831c19b1aa96bbd609
marlageha <marla.geha@yale.edu>
2022-05-19 11:13:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="dmost",
+    version="0.1",
+    author="Marla Geha",
+    author_email="marla.geha@yale.edu",
+    description="Reduce 1D DEIMOS data",
+    packages=setuptools.find_packages(),
+)
