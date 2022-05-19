91f073af0c89d5ea74ac7017604713102a2caf13
marlageha <mgeha@Marlas-MacBook-Pro.local>
2022-05-19 13:52:37 -0400

    update imports

--- dmost/__init__.py
+++ dmost/__init__.py
@@ -1,4 +1,8 @@
 """
 This is the top directory of the dmost package
 """
-from .core import dmost_utils,dmost_create_maskfile,dmost_flexure,dmost_telluric,dmost_chi2_template
+from .core import dmost_utils
+from .core import dmost_create_maskfile, dmost_chip_gap
+from .core import dmost_flexure, dmost_telluric, dmost_chi2_template
+from .core import dmost_emcee, dmost_coadd_emcee, dmost_EW
+
