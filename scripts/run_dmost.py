cb10d33368e4dfcb0361b37aa4e9a16240cd5412
marlageha <mgeha@Marlas-MBP.wireless.yale.internal>
2022-05-19 14:39:40 -0400

    update imports

--- scripts/run_dmost.py
+++ scripts/run_dmost.py
@@ -12,11 +12,7 @@ from astropy.io import ascii
 
 from dmost import *
 
-#import dmost_utils,dmost_flexure, dmost_create_maskfile
-#import dmost_telluric, dmost_chi2_template,dmost_chip_gap, dmost_emcee
-#import dmost_coadd_emcee, dmost_combine_exp, dmost_EW
-
-from dmost.dmost_create_maskfile import write_dmost
+from dmost.core.dmost_create_maskfile import write_dmost 
 
 
 def run_dmost(maskname, rerun_chi2 = 0, rerun_emcee = 0, rerun_coadd = 0):
