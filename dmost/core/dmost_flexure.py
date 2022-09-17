49576f8709d36cc9789503e544b3b10bff9fb4f9
marlageha <mgeha@marlas-mbp.lan>
2022-09-16 21:48:15 -0400

    add flexure coadd

--- dmost/core/dmost_flexure.py
+++ dmost/core/dmost_flexure.py
@@ -414,11 +414,11 @@ def write_spec1d_flexure(data_dir,slits,mask):
 
 
 
-        collate1d = 'pypeit_collate_1d --spec1d_files Science_flex/spec1d_*fits --toler 1.5'
-        os.system(collate1d)
-        os.system('mkdir collate1d_flex')
-        os.system('mv J* collate1d_flex')    
-    
+    collate1d = 'pypeit_collate_1d --spec1d_files Science_flex/spec1d_*fits --toler 1.5'
+    os.system(collate1d)
+    os.system('mkdir collate1d_flex')
+    os.system('mv J* collate1d_flex')    
+
 
   
     return
