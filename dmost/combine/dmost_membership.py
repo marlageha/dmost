d623b357fe2ea55655219d6b54d1d29a2f7d0389
marlageha <marla.geha@yale.edu>
2025-04-01 10:08:17 -0400

    update pm prob

--- dmost/combine/dmost_membership.py
+++ dmost/combine/dmost_membership.py
@@ -21,23 +21,19 @@ from dmost.combine import dmost_vdisp
 
 DEIMOS_RAW     = os.getenv('DEIMOS_RAW')
 
-def set_cmd_lims(rmag):
-    
-    m=(rmag > 10)& (rmag < 30)
-    rmag=rmag[m]
-    
-    try:
-        cmin = np.max(rmag)+0.25
-        cmax = np.min(rmag[rmag >0])-0.25 
-    except:
-        cmin,cmax = 24,13
 
-    if cmin > 24:
-        cmin = 24
-    if cmax < 13:
-        cmax = 14
 
-    return cmin,cmax
+######################################################
+def mg_wmean(x, x_err):
+    """
+    Return the weighted average and standard deviation.
+    values, weights -- NumPy ndarrays with the same shape.
+    """
+    weights = 1./x_err
+    avg     = np.average(x, weights=weights)
+    var     = np.average((x-avg)**2, weights=weights)
+    
+    return (avg, np.sqrt(var))
 
 
 
@@ -73,27 +69,81 @@ def plot_isochrone_HB(dist):
     
     return r_iso,gr_iso 
 
-######################################################
-def mg_wmean(x, x_err):
-    """
-    Return the weighted average and standard deviation.
-
-    values, weights -- NumPy ndarrays with the same shape.
-    """
-    weights = 1./x_err
-    avg     = np.average(x, weights=weights)
-    var     = np.average((x-avg)**2, weights=weights)
-    
-    return (avg, np.sqrt(var))
-
 
 ######################################################
-def membership_v_crude(alldata,this_obj,cmd_mem=0,sig=5.):
+def membership_CMD(alldata,this_obj,cmd_min = 0.25):
 
+    # GET ISOCHRONE PROPERTIES    
+    dist= this_obj['Dist_kpc']
+    feh = this_obj['feh_guess']
+
+    r,gr       = plot_isochrone_parsec_decam(dist,feh)
+    r_hb,gr_hb = plot_isochrone_HB(dist)
+       
+    dmin, emin = [],[]
+    for star in alldata:
+        err = np.sqrt(star['gmag_err']**2 + star['rmag_err']**2)
+
+        d  = (r - star['rmag_o'])**2 + (gr - (star['gmag_o'] - star['rmag_o']))**2
+        d2 = (r_hb - star['rmag_o'])**2 + (gr_hb - (star['gmag_o'] - star['rmag_o']))**2
+
+        tmp = np.min(d)
+        tmp2=np.min(d2)        
+        if tmp2 < tmp:
+            tmp=tmp2
+
+        dmin.append(np.sqrt(tmp))
+        emin.append(err)
+            
+    #  CMD Prob PLUS PHOTOMETRIC ERROR
+    texp     = np.array(dmin)**2 / (2*(np.array(emin)**2 + cmd_min**2))
+    Pmem_cmd = np.exp(-1.*texp)
  
-    # MIN MAX     
-    min_v = this_obj['v_guess'] - this_obj['s_guess']*sig
-    max_v = this_obj['v_guess'] + this_obj['s_guess']*sig
+    return Pmem_cmd
+
+
+######################################################
+# REMOVE STARS WITH STRONG NaI lines
+# Criteria based on Schiavon+1997
+def membership_NaI(alldata, this_obj):
+
+    naI_lim =  1.
+
+    m_rmv    = ((alldata['ew_naI'] - alldata['ew_naI_err']) > naI_lim) & (alldata['MV_o'] < 4)
+    Pmem_NaI = ~m_rmv
+
+    return Pmem_NaI
+
+
+
+######################################################
+# REMOVE FOREGROUND STARS WITH LARGE GAIA PARALLAX 
+def membership_parallax(alldata, this_obj):
+
+    nstar         = np.size(alldata)
+    Pmem_parallax = np.ones(nstar)
+
+    obj_parallax  = 1./this_obj['Dist_kpc']
+
+    # ONLY TEST STARS WITH AVAILABLE PARALLAX
+    m_parallax   =(alldata['gaia_parallax_err'] > 0)
+
+
+    # REMOVE STARS WITH LARGE MEASURED PARALLAX
+    parallax_criteria = alldata['gaia_parallax'] - 3. * alldata['gaia_parallax_err']
+    mrm               = parallax_criteria > 2. * obj_parallax        
+    Pmem_parallax     = ~(mrm & m_parallax)
+
+    return Pmem_parallax
+
+
+
+######################################################
+def membership_v_crude(alldata,this_obj):
+
+    # NOT USED TO DETERMINE ANY PROPERTIES     
+    min_v = this_obj['v_guess'] - 25
+    max_v = this_obj['v_guess'] + 25
 
     Pmem_crude_v = (alldata['v'] > min_v) & (alldata['v'] < max_v)
 
@@ -132,97 +182,54 @@ def membership_vdisp(alldata,this_obj,Pmem_tmp,crude_cut=25.):
 
 
 ######################################################
-# REMOVE STARS WITH STRONG NaI lines
-def membership_NaI(alldata, this_obj):
-
-    naI_lim =  1.
-
-    m_rmv    = ((alldata['ew_naI'] - alldata['ew_naI_err']) > naI_lim) & (alldata['MV_o'] < 4)
-    Pmem_NaI = ~m_rmv
-
-    return Pmem_NaI
-
-######################################################
-def membership_MgI(alldata):
-
-    y = 0.26*alldata['ew_cat']  - 0.5206
-
-    mgI_lim1 = (alldata['ew_mgI'] - alldata['ew_mgI_err'] > 0.5)  & (alldata['ew_cat'] < 4)
-    mgI_lim2 = (alldata['ew_mgI'] > y)  & (alldata['ew_cat'] >= 4)
-
-
-    Pmem_MgI = ~(mgI_lim1 | mgI_lim2)
-
-    return Pmem_MgI
-
+#def membership_MgI(alldata):
+#    y = 0.26*alldata['ew_cat']  - 0.5206
+#    mgI_lim1 = (alldata['ew_mgI'] - alldata['ew_mgI_err'] > 0.5)  & (alldata['ew_cat'] < 4)
+#    mgI_lim2 = (alldata['ew_mgI'] > y)  & (alldata['ew_cat'] >= 4)
+#    Pmem_MgI = ~(mgI_lim1 | mgI_lim2)
+#    return Pmem_MgI
 
 
 ######################################################
 # REMOVE FOREGROUND STARS WITH LARGE PROPER MOTIONS
-# USE GUESS PM, ELSE 
-def membership_PM(alldata, this_obj, Pmem):
+# USE GUESS PM
+def membership_PM(alldata, this_obj, Pmem_tmp):
+
+    Pmem_pm = np.ones(np.size(Pmem_tmp))
 
     # IF THERE ARE ENOUGH MEASURED PMs
-    m_pm    = alldata['gaia_pmra_err'] > 0
-    Pmem_pm = Pmem
-    if (np.sum(m_pm&Pmem) > 0):
+    m_pm    = (alldata['gaia_pmra_err'] > 0) & (alldata['gaia_pmdec_err'] > 0)
+    mem_tmp = Pmem_tmp > 0.5
+    if (np.sum(m_pm&mem_tmp) > 0):
+
+        Pcrude_v        = membership_v_crude(alldata,this_obj)
 
 
         # FOR ALL STARS WITH PMs, CALC AN AVERGAGE WITH REJECTION
-        ra_tmp  = sigma_clip(alldata['gaia_pmra'][Pmem&m_pm], sigma=2, maxiters=3)
-        dec_tmp = sigma_clip(alldata['gaia_pmdec'][Pmem&m_pm], sigma=2, maxiters=3,cenfunc='mean')
+        pra_tmp  = sigma_clip(alldata['gaia_pmra'][mem_tmp&m_pm&Pcrude_v], sigma=2, maxiters=3)
+        pdec_tmp = sigma_clip(alldata['gaia_pmdec'][mem_tmp&m_pm&Pcrude_v], sigma=2, maxiters=3,cenfunc='mean')
         
-        ra_mean, ra_std  = np.mean(ra_tmp),np.std(ra_tmp)
-        dec_mean,dec_std= np.mean(dec_tmp),np.std(dec_tmp)
+        pra_mean, pra_std  = np.mean(pra_tmp),np.std(pra_tmp)
+        pdec_mean,pdec_std = np.mean(pdec_tmp),np.std(pdec_tmp)
 
-        # DETERMINE DISTANCE FROM PM CENTER  -- UNITS DON"T MATTER
-        c_pm    = SkyCoord(ra_mean*u.arcsec,dec_mean*u.arcsec)
-        c_stars = SkyCoord(alldata['gaia_pmra']*u.arcsec,alldata['gaia_pmdec']*u.arcsec) 
-        
+        cos_dec  = np.cos((np.pi/180.)*this_obj['Dec'])
+        diff2    = ((pra_mean - alldata['gaia_pmra'])*cos_dec)**2 + (pdec_mean - alldata['gaia_pmdec'])**2
+        pm_sig   =  np.sqrt((pra_std*cos_dec)**2 + pdec_std**2)
+
+
         # REJECT STARS WITH ERROR CIRCLES FAR FROM PM CENTER
-        sep  = c_stars.separation(c_pm)
-        m_pm_reject = sep > (3*np.sqrt(ra_std**2 + dec_std**2)*u.arcsec) + \
-                        (u.arcsec*np.sqrt(alldata['gaia_pmra_err']**2 + alldata['gaia_pmdec_err']**2))
-        
-        Pmem_pm = ~(m_pm_reject)
+        pmerr = np.sqrt((alldata['gaia_pmra_err']*cos_dec)**2 + alldata['gaia_pmdec_err']**2)*u.arcsec
+
+
+        pm_min = 2.0
+        texp     = np.array(diff2 / (2*(np.array(pmerr)**2 + pm_min**2)))
+        Pmem_pm[m_pm] = np.exp(-1.*texp[m_pm])
+
 
     return Pmem_pm
 
 
 
-######################################################
-def membership_CMD(alldata,this_obj,cmd_min = 0.2):
-
-    # GET ISOCHRONE PROPERTIES    
-    dist= this_obj['Dist_kpc']
-    feh = this_obj['feh_guess']
-
-    r,gr       = plot_isochrone_parsec_decam(dist,feh)
-    r_hb,gr_hb = plot_isochrone_HB(dist)
-       
-    dmin, emin = [],[]
-    for star in alldata:
-        err = np.sqrt(star['gmag_err']**2 + star['rmag_err']**2)
-
-        d  = (r - star['rmag_o'])**2 + (gr - (star['gmag_o'] - star['rmag_o']))**2
-        d2 = (r_hb - star['rmag_o'])**2 + (gr_hb - (star['gmag_o'] - star['rmag_o']))**2
-
-        tmp = np.min(d)
-        tmp2=np.min(d2)        
-        if tmp2 < tmp:
-            tmp=tmp2
-
-          
-        dmin.append(np.sqrt(tmp))
-        emin.append(err)
-            
-
-    #  CMD Prob PLUS PHOTOMETRIC ERROR
-    texp     = np.array(dmin)**2 / (2*(np.array(emin)**2 + cmd_min**2))
-    Pmem_cmd = np.exp(-1.*texp)
- 
-
-    return Pmem_cmd
 
 
 ######################################################
@@ -273,27 +280,6 @@ def flag_velocity_outliers(alldata,Pmem, low=3,high=3):
 
 
 ######################################################
-# REMOVE FOREGROUND STARS WITH LARGE GAIA PARALLAX 
-def membership_parallax(alldata, this_obj):
-
-    nstar         = np.size(alldata)
-    Pmem_parallax = np.ones(nstar)
-
-    obj_parallax  = 1./this_obj['Dist_kpc']
-
-    # ONLY TEST STARS WITH AVAILABLE PARALLAX
-    m_parallax   =(alldata['gaia_parallax_err'] > 0)
-
-
-    # REMOVE STARS WITH LARGE MEASURED PARALLAX
-    parallax_criteria = alldata['gaia_parallax'] - 3. * alldata['gaia_parallax_err']
-    mrm               = parallax_criteria > 2. * obj_parallax        
-    Pmem_parallax     = ~(mrm & m_parallax)
-
-    return Pmem_parallax
-
-
-######################################################
 def flag_good_data(alldata):
 
     m_err = (alldata['v_err'] > 15) | (alldata['v_err'] < 0)
@@ -314,31 +300,31 @@ def find_members(alldata,this_obj):
 
     flag_good       = flag_good_data(alldata)
     Pmem_cmd        = membership_CMD(alldata, this_obj)
-
-    flag_parallax   = membership_parallax(alldata, this_obj)
-    flag_NaI        = membership_NaI(alldata, this_obj)
-#    flag_MgI        = membership_MgI(alldata)
-
-    all_flags =  flag_parallax&flag_NaI&flag_good
+    Pmem_EW         = membership_NaI(alldata, this_obj)
+    Pmem_parallax   = membership_parallax(alldata, this_obj)
 
 
-#    Pmem_pm         = membership_PM(alldata, this_obj, Pmem_cmd*all_flags)
+    all_flags1      = flag_good * Pmem_cmd  * Pmem_EW * Pmem_parallax
+    Pmem_pm         = membership_PM(alldata, this_obj, all_flags1)
 
-    Pmem_v          = membership_vdisp(alldata,this_obj,Pmem_cmd*all_flags)
-    #Pmem_feh        = membership_v_crude(alldata, this_obj)
+
+    all_flags2      = all_flags1 * Pmem_pm
+    Pmem_v          = membership_vdisp(alldata,this_obj,all_flags2)
+
+    #Pmem_feh        = membership_feh(alldata, this_obj,all_flags2*Pmem_v)
 
 
     # FLAG HB AND SET FEH TO ZERO
     flag_HB       = flag_HB_stars(alldata,this_obj)
     flag_var      = flag_variable_stars(alldata)
 
-    Pmem_inclusive   =  Pmem_cmd * Pmem_v * all_flags
+    Pmem_inclusive   =  all_flags2 * Pmem_v 
 
 
     Pmem      =  Pmem_inclusive * ~flag_var 
 
 
-    return Pmem_inclusive, Pmem#, Pmem_cmd, Pmem_crude_v, Pmem_NaI, Pmem_pm, Pmem_parallax, Pmem_MgI, Pmem_v
+    return Pmem_inclusive, Pmem#, Pmem_pm,Pmem_v,Pmem_feh
 
 
 
