f287bd585594421289a30d47ba46323a234f3899
marlageha <marla.geha@yale.edu>
2025-04-08 10:03:33 -0400

    update

--- dmost/core/dmost_coadd_emcee.py
+++ dmost/core/dmost_coadd_emcee.py
@@ -115,7 +115,7 @@ def lnprob_v(theta, wave, flux, ivar, twave,tflux, pflux,pwave,npoly,pfit):
 def lnprior_v(theta):
    
     #v = theta[0],  w = theta[1]
-    if (-600. < theta[0] < 500.) & (-60. < theta[1] < 60.):
+    if (-700. < theta[0] < 700.) & (-60. < theta[1] < 60.):
         return 0.0
     
     return -np.inf
