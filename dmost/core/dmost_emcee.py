72ef81b8580ca87c2e8fe3ded33a4d4d0a01e71a
marlageha <marla.geha@yale.edu>
2025-05-28 11:26:47 -0400

    update SN

--- dmost/core/dmost_emcee.py
+++ dmost/core/dmost_emcee.py
@@ -463,7 +463,8 @@ def run_emcee(data_dir, slits, mask, outfile, clobber=0):
         telluric = Table.read(tfile[0])
 
         # WRITE TO SCREEN
-        SNmin = 2.
+        SNmin = 1.5
+
         m = (slits['SN'][:,ii] > SNmin) & (slits['marz_flag'] < 2)
         nslits = np.sum(m)
         dmost_utils.printlog(log,'{} {} Emcee with {} slits w/SN > {}'.format(mask['maskname'][0],\
